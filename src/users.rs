9abde0fcc5cd5f929199959e991c130be10eb9ac
ryonakaya <nakayaryo0716@gmail.com>
2024-11-21 16:47:23 +0900

    [fix]chenge to User, PubUserInfo have newtype + fix to match

--- src/users.rs
+++ src/users.rs
@@ -1,23 +1,84 @@
 use async_trait::async_trait;
-use serde::Serialize;
-use sqlx::{prelude::FromRow, MySql, Pool};
+use serde::{Deserialize, Serialize};
+use sqlx::{mysql::MySqlRow, prelude::FromRow, MySql, Pool, Row};
 use thiserror::Error;
 use uuid::Uuid;
 
 use crate::{util::HashFunc, RepositoryTargetReader, RepositoryWriter};
 
-pub struct CreateUserPayload {
-    pub user_name: String,
-    pub mail: String,
-    pub password: String,
+#[derive(Debug, Clone, Serialize, FromRow, PartialEq)]
+pub struct UserId(String);
+
+impl From<String> for UserId {
+    fn from(value: String) -> Self {
+        Self(value.to_string())
+    }
 }
 
-#[derive(Debug, Clone, FromRow, PartialEq)]
+impl From<UserId> for String {
+    fn from(value: UserId) -> Self {
+        value.0
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow, PartialEq)]
+pub struct UserName(String);
+
+impl From<String> for UserName {
+    fn from(value: String) -> Self {
+        Self(value.to_string())
+    }
+}
+
+impl From<UserName> for String {
+    fn from(value: UserName) -> Self {
+        value.0
+    }
+}
+
+#[derive(Debug, Clone, FromRow, Deserialize, PartialEq)]
+pub struct Mail(String);
+
+impl From<String> for Mail {
+    fn from(value: String) -> Self {
+        Self(value.to_string())
+    }
+}
+
+impl From<Mail> for String {
+    fn from(value: Mail) -> Self {
+        value.0
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, FromRow, PartialEq)]
+pub struct Password(String);
+
+impl From<String> for Password {
+    fn from(value: String) -> Self {
+        Self(value.to_string())
+    }
+}
+
+impl From<Password> for String {
+    fn from(value: Password) -> Self {
+        value.0
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, PartialEq)]
+pub struct CreateUserPayload {
+    pub user_name: UserName,
+    pub mail: Mail,
+    pub password: Password,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct User {
-    user_id: String,
-    user_name: String,
-    mail: String,
-    password: String,
+    user_id: UserId,
+    user_name: UserName,
+    mail: Mail,
+    password: Password,
 }
 
 impl User {
@@ -26,18 +87,38 @@ impl User {
         hasher: Box<dyn HashFunc>,
     ) -> Result<Self, Box<dyn std::error::Error>> {
         Ok(Self {
-            user_id: Uuid::new_v4().to_string(),
+            user_id: UserId::from(Uuid::new_v4().to_string()),
             user_name: payload.user_name,
             mail: payload.mail,
-            password: hasher.call(&payload.password)?,
+            password: Password::from(hasher.call(&payload.password.0)?),
         })
     }
 }
 
-#[derive(Debug, Clone, Serialize, FromRow)]
+impl FromRow<'_, MySqlRow> for User {
+    fn from_row(row: &'_ MySqlRow) -> Result<Self, sqlx::Error> {
+        Ok(User {
+            user_id: UserId(row.try_get("user_id")?),
+            user_name: UserName(row.try_get("user_name")?),
+            mail: Mail(row.try_get("mail")?),
+            password: Password(row.try_get("password")?),
+        })
+    }
+}
+
+#[derive(Debug, Clone, Serialize)]
 pub struct PubUserInfo {
-    pub user_id: String,
-    pub user_name: String,
+    pub user_id: UserId,
+    pub user_name: UserName,
+}
+
+impl FromRow<'_, MySqlRow> for PubUserInfo {
+    fn from_row(row: &'_ MySqlRow) -> Result<Self, sqlx::Error> {
+        Ok(PubUserInfo {
+            user_id: UserId(row.try_get("user_id")?),
+            user_name: UserName(row.try_get("user_name")?),
+        })
+    }
 }
 
 #[derive(Debug, Clone, Error)]
@@ -62,17 +143,18 @@ impl UserRepository {
                 (user_id, user_name, mail, password) VALUES (?, ?, ?, ?)
             "#,
         )
-        .bind(&payload.user_id)
-        .bind(&payload.user_name)
-        .bind(&payload.mail)
-        .bind(&payload.password)
+        .bind::<String>(payload.user_id.clone().into())
+        .bind::<String>(payload.user_name.clone().into())
+        .bind::<String>(payload.mail.clone().into())
+        .bind::<String>(payload.password.clone().into())
         .execute(&self.pool)
         .await
         .map_err(|_e| UserError::NotFound)?;
         Ok(())
     }
 
-    async fn execute_query(&self, id: &str) -> Result<PubUserInfo, UserError> {
+    async fn execute_query(&self, id: &UserId) -> Result<PubUserInfo, UserError> {
+        println!("{:?}", id);
         let query_res = sqlx::query_as(
             r#"
                 SELECT user_id, user_name
@@ -80,7 +162,7 @@ impl UserRepository {
                 WHERE user_id = ?
             "#,
         )
-        .bind(id)
+        .bind::<String>(id.clone().into())
         .fetch_one(&self.pool)
         .await
         .map_err(|_e| UserError::NotFound)?;
@@ -98,24 +180,24 @@ impl UserRepository {
                 WHERE user_id = ?
             "#,
         )
-        .bind(&payload.user_name)
-        .bind(&payload.mail)
-        .bind(&payload.password)
-        .bind(&payload.user_id)
+        .bind::<String>(payload.user_name.clone().into())
+        .bind::<String>(payload.mail.clone().into())
+        .bind::<String>(payload.password.clone().into())
+        .bind::<String>(payload.user_id.clone().into())
         .execute(&self.pool)
         .await
         .map_err(|_e| UserError::NotFound)?;
         Ok(())
     }
 
-    async fn execute_delete_query(&self, id: &str) -> Result<(), UserError> {
+    async fn execute_delete_query(&self, id: &UserId) -> Result<(), UserError> {
         sqlx::query(
             r#"
                 DELETE FROM user_table
                 WHERE user_id = ?
             "#,
         )
-        .bind(id.to_owned())
+        .bind::<String>(id.clone().into())
         .execute(&self.pool)
         .await
         .map_err(|_e| UserError::NotFound)?;
@@ -124,17 +206,17 @@ impl UserRepository {
 }
 
 #[async_trait]
-impl RepositoryTargetReader<String> for UserRepository {
+impl RepositoryTargetReader<UserId> for UserRepository {
     type QueryRes = PubUserInfo;
     type QueryErr = UserError;
 
-    async fn read(&self, id: &String) -> Result<Self::QueryRes, Self::QueryErr> {
+    async fn read(&self, id: &UserId) -> Result<Self::QueryRes, Self::QueryErr> {
         self.execute_query(id).await
     }
 }
 
 #[async_trait]
-impl<'a> RepositoryWriter<'a, '_, User, String> for UserRepository {
+impl<'a> RepositoryWriter<'a, '_, User, UserId> for UserRepository {
     type Output = PubUserInfo;
     type Error = UserError;
 
@@ -144,12 +226,12 @@ impl<'a> RepositoryWriter<'a, '_, User, String> for UserRepository {
         Ok(query_res)
     }
 
-    async fn update(&self, _id: &'a String, payload: &User) -> Result<Self::Output, Self::Error> {
+    async fn update(&self, _id: &'a UserId, payload: &User) -> Result<Self::Output, Self::Error> {
         self.execute_update_query(payload).await?;
         let query_res = self.read(&payload.user_id).await?;
         Ok(query_res)
     }
-    async fn delete(&self, id: &'a String) -> Result<(), Self::Error> {
+    async fn delete(&self, id: &'a UserId) -> Result<(), Self::Error> {
         self.execute_delete_query(id).await
     }
 }
@@ -165,11 +247,10 @@ mod test {
         RepositoryTargetReader, RepositoryWriter,
     };
 
-    use super::UserRepository;
+    use super::{Mail, Password, UserId, UserName, UserRepository};
 
     async fn set_up_db() -> UserRepository {
         let db_url = dotenvy::var("DATABASE_URL").unwrap();
-        println!("{}", db_url);
         let pool = MySqlPool::connect(&db_url).await.unwrap();
         UserRepository { pool }
     }
@@ -177,9 +258,9 @@ mod test {
     fn user_provider() -> User {
         let num = random::<i32>();
         let payload = CreateUserPayload {
-            user_name: format!("test_user_name_{}", num),
-            mail: format!("test_user_mail_{}@mail.com", num),
-            password: format!("test_user_pass_{}", num),
+            user_name: UserName::from(format!("test_user_name_{}", num)),
+            mail: Mail::from(format!("test_user_mail_{}@mail.com", num)),
+            password: Password::from(format!("test_user_pass_{}", num)),
         };
 
         let hasher = Box::new(default_hash_password);
@@ -190,13 +271,13 @@ mod test {
         let num = random::<i32>();
         User {
             user_id: user.user_id,
-            user_name: format!("test_user_name_{}", num),
-            mail: format!("test_user_mail_{}@mail.com", num),
-            password: format!("test_user_pass_{}", num),
+            user_name: UserName::from(format!("test_user_name_{}", num)),
+            mail: Mail::from(format!("test_user_mail_{}@mail.com", num)),
+            password: Password::from(format!("test_user_pass_{}", num)),
         }
     }
 
-    async fn query_full_data(id: &str) -> Result<User, Box<dyn std::error::Error>> {
+    async fn query_full_data(id: &UserId) -> Result<User, Box<dyn std::error::Error>> {
         let repo = set_up_db().await;
         let res = query_as(
             r#"
@@ -205,7 +286,7 @@ mod test {
                 WHERE user_id = ?
             "#,
         )
-        .bind(id)
+        .bind::<String>(id.clone().into())
         .fetch_one(&repo.pool)
         .await
         .map_err(|e| e)?;
@@ -217,7 +298,9 @@ mod test {
         let repo = set_up_db().await;
         let new_user = user_provider();
 
-        let user_info = repo.insert(&new_user).await.unwrap();
+        repo.insert(&new_user).await.unwrap();
+        let user_info = query_full_data(&new_user.user_id).await.unwrap();
+
         assert_eq!(user_info.user_id, new_user.user_id);
         assert_eq!(user_info.user_name, new_user.user_name);
 
